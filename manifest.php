5f564de4041259d711a47a11a59987cbbee7281a
bugalot <jerome@taotesting.com>
2018-05-24 16:25:40 +0200

    First skeletton

---
+++ manifest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA.
+ */
+
+return [
+    'name' => 'taoMobileApp',
+    'label' => 'TAO Mobile App',
+    'description' => 'Extension providing server-side components for the TAO Mobile App.',
+    'license' => 'Proprietary',
+    'version' => '0.1.0',
+    'author' => 'Open Assessment Technologies',
+    'requires' => [
+        'tao' => '>=19.1.0'
+    ],
+    'acl' => [],
+    'routes' => [],
+    'update' => 'oat\\taoMobileApp\\scripts\\update\\Updater'
+];
---
+++ scripts/update/Updater.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA.
+ */
+
+namespace oat\taoMobileApp\scripts\update;
+
+/**
+ * TAO Mobile App Extension Updater.
+ * 
+ * This class provides an implementation of the Generis
+ * Extension Updater aiming at updating the TAO Mobile App Extension.
+ */
+class Updater extends \common_ext_ExtensionUpdater {
+
+    /**
+     * Update the Extension
+     * 
+     * Calling this method will update the TAO Mobile App Extension from
+     * an $initialVersion to a target version.
+     * 
+     * @param string $initialVersion
+     * @see \common_ext_ExtensionUpdater
+     * @return void
+     */
+    public function update($initialVersion) {
+        
+    }
+}
