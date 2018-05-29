9e9221ad332384452c1e9907be13bba3518c1935
bugalot <jerome@taotesting.com>
2018-05-29 13:30:05 +0200

    add dependency to taoDeliveryRdf

--- manifest.php
+++ manifest.php
@@ -12,7 +12,8 @@ return [
     'author' => 'Open Assessment Technologies',
     'requires' => [
         'taoCe' => '3.18.0',
-        'tao' => '>=19.1.0'
+        'tao' => '>=19.1.0',
+        'taoDeliveryRdf' => '5.0.0'
     ],
     'install' => [
         'php' => [
