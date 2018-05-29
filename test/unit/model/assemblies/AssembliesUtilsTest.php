995cfc8a6d024a15a3125db9c0c33f814ef87fc7
bugalot <jerome@taotesting.com>
2018-05-29 13:31:36 +0200

    Added proper unit tests.

--- test/unit/model/assemblies/AssembliesUtilsTest.php
+++ test/unit/model/assemblies/AssembliesUtilsTest.php
@@ -6,14 +6,81 @@ use oat\taoMobileApp\model\assemblies\AssembliesUtils;
 
 class AssembliesUtilsTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTransformToMobileAssembly()
+    private $path;
+
+    public function setUp()
     {
-        $archivePath = __DIR__ . '/../../../samples/assemblies/assembly-simple-languages.zip';
-        $testArchivePath = __DIR__ . '/../../../samples/assemblies/assembly-simple-test.zip';;
+        // Setup a filesystem environment.
+        $tmp = sys_get_temp_dir();
+        $this->path = "${tmp}/.taoMobileApp/test/assemblies/utils";
+        @mkdir($this->path, 0777, true);
+    }
+
+    protected function tearDown()
+    {
+        \tao_helpers_File::remove($this->path, true);
+    }
+
+    public function testTransformToMobileAssemblySimple()
+    {
+        // Prepare...
+        $archivePath = __DIR__ . '/../../../samples/assemblies/assembly-simple.zip';
+        $testArchivePath = $this->path . '/assembly-simple-test.zip';;
         copy($archivePath, $testArchivePath);
         $zipArchive = new \ZipArchive();
         $zipArchive->open($testArchivePath, \ZipArchive::CREATE);
         AssembliesUtils::transformToMobileAssembly($zipArchive);
+        $zipArchive->close();
+
+        // Test on a flushed archive.
+        $zipArchive = new \ZipArchive();
+        $zipArchive->open($testArchivePath, \ZipArchive::CREATE);
+
+        $this->assertFalse($zipArchive->getFromName('delivery.rdf'));
+        $this->assertFalse($zipArchive->getFromName('manifest.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/item.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/portableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/variableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/oat-300x150.png'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/tao-user-styles.css'));
+        $this->assertNotFalse($zipArchive->getFromName('Q02/item.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q02/portableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q02/variableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q03/item.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q03/portableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q03/variableElements.json'));
+
+        $zipArchive->close();
+    }
+
+    public function testTransformToMobileAssemblyMultipleLanguages()
+    {
+        // Prepare...
+        $archivePath = __DIR__ . '/../../../samples/assemblies/assembly-simple-languages.zip';
+        $testArchivePath = $this->path . '/assembly-simple-test.zip';;
+        copy($archivePath, $testArchivePath);
+        $zipArchive = new \ZipArchive();
+        $zipArchive->open($testArchivePath, \ZipArchive::CREATE);
+        AssembliesUtils::transformToMobileAssembly($zipArchive);
+        $zipArchive->close();
+
+        // Test on a flushed archive.
+        $zipArchive = new \ZipArchive();
+        $zipArchive->open($testArchivePath, \ZipArchive::CREATE);
+
+        $this->assertFalse($zipArchive->getFromName('delivery.rdf'));
+        $this->assertFalse($zipArchive->getFromName('manifest.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/item.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/portableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/variableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/oat-300x150.png'));
+        $this->assertNotFalse($zipArchive->getFromName('Q01/tao-user-styles.css'));
+        $this->assertNotFalse($zipArchive->getFromName('Q02/item.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q02/portableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q02/variableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q03/item.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q03/portableElements.json'));
+        $this->assertNotFalse($zipArchive->getFromName('Q03/variableElements.json'));
 
         $zipArchive->close();
     }
