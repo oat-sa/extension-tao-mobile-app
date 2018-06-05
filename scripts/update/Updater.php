e89988885d3c28d0c7492f5c0ea7065514e48231
bugalot <jerome@taotesting.com>
2018-06-05 16:07:25 +0200

    remove updater

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -24,28 +24,9 @@ class Updater extends \common_ext_ExtensionUpdater {
      * 
      * @param string $initialVersion
      * @see \common_ext_ExtensionUpdater
-     * @throws \common_Exception
      * @return void
      */
     public function update($initialVersion) {
 
-        if ($this->isVersion('0.1.0')) {
-
-            $assemblerService = new MobileAssemblerService();
-            $this->getServiceManager()->register(MobileAssemblerService::SERVICE_ID, $assemblerService);
-
-            $deliveryExecutionDelete = new DeliveryExecutionDeleteService([
-                DeliveryExecutionDeleteService::OPTION_DELETE_DELIVERY_EXECUTION_DATA_SERVICES => [
-                    'taoQtiTest/ExtendedStateService',
-                    'taoQtiTest/TestSessionService',
-                    'taoQtiTest/QtiTimerFactory',
-                    'taoQtiTest/QtiRunnerService',
-                ],
-            ]);
-
-            $this->getServiceManager()->register(DeliveryExecutionDeleteService::SERVICE_ID, $deliveryExecutionDelete);
-
-            $this->setVersion('1.0.0');
-        }
     }
 }
